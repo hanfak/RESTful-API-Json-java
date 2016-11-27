3e73df7ef8c31000f69fc4c198370c9e734bca38
Hanif Fakira <fakira.work@gmail.com>
2016-11-27 22:06:39 +0000

    US1 commented out method to combine path mehtods

--- src/main/java/com/hanfak/greedydb/controllers/ImportController.java
+++ src/main/java/com/hanfak/greedydb/controllers/ImportController.java
@@ -30,4 +30,27 @@ public class ImportController {
 	public List<Employer> getEmployees() {
 		return databaseService.getEmployerObjects();
 	}
+	
+//  Tried to use variable pathparams, and return response, but cannot
+//	solve the error. It is getting the data from db and storing in model, but
+//	does not turn to json
+//	@GET
+//	@Path("/{streamName}")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public Response getObjects(
+//			@PathParam("streamName") String streamName) {
+//		System.out.println((String) streamName == "click");
+//		if(streamName.equals("click")){
+//			System.out.println("click");
+//			List<Click> clicks = databaseService.getClicks();
+//			System.out.println(clicks.get(0).getTimestamp());
+//			return Response.ok(clicks).build();
+//		}
+//		if(streamName.equals("employer")){
+//			System.out.println("employer");
+//			List<Employer> employers = databaseService.getEmployerObjects();
+//			System.out.println(employers.get(0).getTimestamp());
+//			return Response.ok(employers).build();
+//		}
+//	}
 }
