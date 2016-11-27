ed9ec40829c5eb3706c16e4008144b0058f0bc52
Hanif Fakira <fakira.work@gmail.com>
2016-11-27 22:49:30 +0000

    US2 refactoring

--- src/main/java/com/hanfak/greedydb/databaseServices/QueryDatabaseService.java
+++ src/main/java/com/hanfak/greedydb/databaseServices/QueryDatabaseService.java
@@ -15,50 +15,47 @@ public class QueryDatabaseService {
 	public String getFieldValue(String streamName,
 			String timestamp, 
 			String jsonPath) {
-		// To connect to mongodb server
-		MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
-		
-		// Now connect to your databases
-		DB db = mongoClient.getDB( "test" );
-		System.out.println("Connect to database successfully");
-		
-		DBCollection coll = db.getCollection("streams");
-		System.out.println("Collection mycol selected successfully");
-
-		BasicDBObject whereQuery = new BasicDBObject();
-		List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
-		obj.add(new BasicDBObject("streamName", streamName));
-		obj.add(new BasicDBObject("timestamp", Integer.parseInt(timestamp)));
-		whereQuery.put("$and", obj);
-		//if statement for streamName
-		//whereQuery.put("streamName", "clicks");
-		System.out.println(whereQuery.toString());
-		DBCursor cursor = coll.find(whereQuery);
-		
-		
 		String value = "";
-
+		DBCursor cursor = findJsonPathQuery(streamName, timestamp);
+		value = retrieveValueOfJsonPath(cursor, jsonPath);
+		cursor.close();
+		return value;
+	}
+	
+	private String retrieveValueOfJsonPath( DBCursor cursor, String jsonPath){
+		String value = "";
 		while (cursor.hasNext()) { 
 			DBObject obj1 = cursor.next();
-			//field path, split into arrayList, then iterate, and perform
-			//another get(path) if arrayList not empty
-			System.out.println(jsonPath);
-			//Get list of strings for jsonpath
 			List<String> items = Arrays.asList(jsonPath.split("\\s*/\\s*"));
-			System.out.println(Arrays.toString(items.toArray()));
-			//for loop 
 			if(items.size() == 2){
 				value = (String) ((DBObject) obj1.get(items.get(0))).get(items.get(1));
 			}
 			if(items.size() == 1) {
 				value = (String)  obj1.get(jsonPath);
 			}
-			System.out.println(value);
 		}
-		
-		cursor.close();
 		return value;
-		}
+	}
 	
+	private DBCollection DatabaseCollection() {
+	    MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+		
+		DB db = mongoClient.getDB( "test" );
+		System.out.println("Connect to database successfully");
+		
+		DBCollection collection = db.getCollection("streams");
+		System.out.println("Collection mycol selected successfully");
+	   
+	    return collection;
+	 }
+    
+	private DBCursor findJsonPathQuery(String streamName, String timestamp) {
+		BasicDBObject whereQuery = new BasicDBObject();
+		List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
+		obj.add(new BasicDBObject("streamName", streamName));
+		obj.add(new BasicDBObject("timestamp", Integer.parseInt(timestamp)));
+		whereQuery.put("$and", obj);
+		return DatabaseCollection().find(whereQuery);
+	}
     
 }
